d38049ab7f1e466a439a4e261062190688ee25e3
Giovani Cavalheri <inocenciogiovanin@gmail.com>
2025-10-24 20:32:24 -0300

    move-pastes

---
+++ Second-Project-React/src/App.jsx
@@ -0,0 +1,15 @@
+export default App;
+
+function App() {
+  return (
+    <div>
+      <h1>Olá Mundo!</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto
+        inventore, reiciendis quae alias expedita pariatur mollitia facilis odit
+        autem, esse voluptates fugiat porro at minus nobis ipsa. Saepe, tenetur
+        repellat.
+      </p>
+    </div>
+  );
+}
