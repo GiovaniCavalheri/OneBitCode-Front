4d8fd786a7d50f7f71f6d79a31c09f29dc67716c
Giovani Cavalheri <inocenciogiovanin@gmail.com>
2025-10-29 00:11:56 -0300

    finish-ex002

--- Fundamentos-React/Ex002-Criando-Card/src/components/Card/index.jsx
+++ Fundamentos-React/Ex002-Criando-Card/src/components/Card/index.jsx
@@ -5,9 +5,9 @@ function Card() {
     return (
       // eslint-disable-next-line no-undef
       <div className={styles.container}>
-        <img src={imgCard} alt="Um poster interativo do Star Wars" />
+        <img className={styles.poster} src={imgCard} alt="Um poster interativo do Star Wars" />
         <div>
-          <h2>Pôster do Filme Star Wars</h2>
+          <h2 className={styles.titlePag}>Pôster: Star Wars(1972)</h2>
           <p>
             Um pôster decorativo épico do filme Star Wars, com moldura de MDF e
             tamanho A3. Uma ótima recordação de um dos mais icônicos filmes de
@@ -16,7 +16,7 @@ function Card() {
             Não perca a chance de adicionar essa linda memória ao seu acervo!
           </p>
 
-          <button>Comprar Agora </button>
+          <button className={styles.button}>Comprar Agora </button>
         </div>
       </div>
     );
